e4a5d6628606a44d2c15d1ae95844c22e0a81580
CharlesTogle <149758871+Luhartt@users.noreply.github.com>
2024-11-11 22:32:35 +0800

    Updates
    *Added Flight Booking (two-way) user control
    *Added Flight Booking main user control
    *Added icons for Flight Booking

---
+++ FLIGHT-RESERVATION/Flight Booking/FlightBooking-FlightDetails/Round-Trip.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace FLIGHT_RESERVATION.Flight_Booking.FlightBooking_FlightDetails
+{
+    partial class Round_Trip
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Round_Trip));
+            this.picDepartureArrivalLocation = new System.Windows.Forms.PictureBox();
+            this.picCompanions = new System.Windows.Forms.PictureBox();
+            this.picClassSeat = new System.Windows.Forms.PictureBox();
+            this.picDepartureReturnDate = new System.Windows.Forms.PictureBox();
+            this.cboDepartureLocation = new System.Windows.Forms.ComboBox();
+            this.cboArrivalLocation = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.picLine = new System.Windows.Forms.PictureBox();
+            this.picAirplaneLift = new System.Windows.Forms.PictureBox();
+            this.picAirplaneLand = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picDepartureArrivalLocation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCompanions)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picClassSeat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDepartureReturnDate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAirplaneLift)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAirplaneLand)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picDepartureArrivalLocation
+            // 
+            this.picDepartureArrivalLocation.Image = ((System.Drawing.Image)(resources.GetObject("picDepartureArrivalLocation.Image")));
+            this.picDepartureArrivalLocation.Location = new System.Drawing.Point(22, 19);
+            this.picDepartureArrivalLocation.Name = "picDepartureArrivalLocation";
+            this.picDepartureArrivalLocation.Size = new System.Drawing.Size(490, 240);
+            this.picDepartureArrivalLocation.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picDepartureArrivalLocation.TabIndex = 0;
+            this.picDepartureArrivalLocation.TabStop = false;
+            // 
+            // picCompanions
+            // 
+            this.picCompanions.Image = ((System.Drawing.Image)(resources.GetObject("picCompanions.Image")));
+            this.picCompanions.Location = new System.Drawing.Point(22, 278);
+            this.picCompanions.Name = "picCompanions";
+            this.picCompanions.Size = new System.Drawing.Size(490, 240);
+            this.picCompanions.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picCompanions.TabIndex = 1;
+            this.picCompanions.TabStop = false;
+            // 
+            // picClassSeat
+            // 
+            this.picClassSeat.Image = ((System.Drawing.Image)(resources.GetObject("picClassSeat.Image")));
+            this.picClassSeat.Location = new System.Drawing.Point(558, 291);
+            this.picClassSeat.Name = "picClassSeat";
+            this.picClassSeat.Size = new System.Drawing.Size(288, 107);
+            this.picClassSeat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picClassSeat.TabIndex = 2;
+            this.picClassSeat.TabStop = false;
+            // 
+            // picDepartureReturnDate
+            // 
+            this.picDepartureReturnDate.Image = ((System.Drawing.Image)(resources.GetObject("picDepartureReturnDate.Image")));
+            this.picDepartureReturnDate.Location = new System.Drawing.Point(548, 19);
+            this.picDepartureReturnDate.Name = "picDepartureReturnDate";
+            this.picDepartureReturnDate.Size = new System.Drawing.Size(304, 257);
+            this.picDepartureReturnDate.TabIndex = 3;
+            this.picDepartureReturnDate.TabStop = false;
+            // 
+            // cboDepartureLocation
+            // 
+            this.cboDepartureLocation.FormattingEnabled = true;
+            this.cboDepartureLocation.Location = new System.Drawing.Point(307, 136);
+            this.cboDepartureLocation.Name = "cboDepartureLocation";
+            this.cboDepartureLocation.Size = new System.Drawing.Size(121, 24);
+            this.cboDepartureLocation.TabIndex = 4;
+            // 
+            // cboArrivalLocation
+            // 
+            this.cboArrivalLocation.FormattingEnabled = true;
+            this.cboArrivalLocation.Location = new System.Drawing.Point(307, 197);
+            this.cboArrivalLocation.Name = "cboArrivalLocation";
+            this.cboArrivalLocation.Size = new System.Drawing.Size(121, 24);
+            this.cboArrivalLocation.TabIndex = 5;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(705, 88);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 6;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(682, 197);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 24);
+            this.comboBox4.TabIndex = 7;
+            // 
+            // picLine
+            // 
+            this.picLine.Image = ((System.Drawing.Image)(resources.GetObject("picLine.Image")));
+            this.picLine.Location = new System.Drawing.Point(46, 136);
+            this.picLine.Name = "picLine";
+            this.picLine.Size = new System.Drawing.Size(431, 2);
+            this.picLine.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picLine.TabIndex = 8;
+            this.picLine.TabStop = false;
+            // 
+            // picAirplaneLift
+            // 
+            this.picAirplaneLift.Image = ((System.Drawing.Image)(resources.GetObject("picAirplaneLift.Image")));
+            this.picAirplaneLift.Location = new System.Drawing.Point(46, 68);
+            this.picAirplaneLift.Name = "picAirplaneLift";
+            this.picAirplaneLift.Size = new System.Drawing.Size(44, 44);
+            this.picAirplaneLift.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picAirplaneLift.TabIndex = 9;
+            this.picAirplaneLift.TabStop = false;
+            // 
+            // picAirplaneLand
+            // 
+            this.picAirplaneLand.Image = ((System.Drawing.Image)(resources.GetObject("picAirplaneLand.Image")));
+            this.picAirplaneLand.Location = new System.Drawing.Point(46, 177);
+            this.picAirplaneLand.Name = "picAirplaneLand";
+            this.picAirplaneLand.Size = new System.Drawing.Size(44, 44);
+            this.picAirplaneLand.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picAirplaneLand.TabIndex = 10;
+            this.picAirplaneLand.TabStop = false;
+            // 
+            // Round_Trip
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.picAirplaneLand);
+            this.Controls.Add(this.picAirplaneLift);
+            this.Controls.Add(this.picLine);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.cboArrivalLocation);
+            this.Controls.Add(this.cboDepartureLocation);
+            this.Controls.Add(this.picDepartureReturnDate);
+            this.Controls.Add(this.picClassSeat);
+            this.Controls.Add(this.picCompanions);
+            this.Controls.Add(this.picDepartureArrivalLocation);
+            this.Name = "Round_Trip";
+            this.Size = new System.Drawing.Size(906, 537);
+            this.Load += new System.EventHandler(this.Round_Trip_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picDepartureArrivalLocation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCompanions)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picClassSeat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDepartureReturnDate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAirplaneLift)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAirplaneLand)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picDepartureArrivalLocation;
+        private System.Windows.Forms.PictureBox picCompanions;
+        private System.Windows.Forms.PictureBox picClassSeat;
+        private System.Windows.Forms.PictureBox picDepartureReturnDate;
+        private System.Windows.Forms.ComboBox cboDepartureLocation;
+        private System.Windows.Forms.ComboBox cboArrivalLocation;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.PictureBox picLine;
+        private System.Windows.Forms.PictureBox picAirplaneLift;
+        private System.Windows.Forms.PictureBox picAirplaneLand;
+    }
+}
