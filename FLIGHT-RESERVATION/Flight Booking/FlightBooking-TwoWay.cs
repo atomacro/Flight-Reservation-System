617f5bc738a80e5967d7599a3e38e244280e4d43
CharlesTogle <149758871+Luhartt@users.noreply.github.com>
2024-11-11 21:43:51 +0800

    Added folder and user control

---
+++ FLIGHT-RESERVATION/Flight Booking/FlightBooking-TwoWay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FLIGHT_RESERVATION
+{
+    public partial class FlightBooking_TwoWay : UserControl
+    {
+        public FlightBooking_TwoWay()
+        {
+            InitializeComponent();
+        }
+    }
+}
