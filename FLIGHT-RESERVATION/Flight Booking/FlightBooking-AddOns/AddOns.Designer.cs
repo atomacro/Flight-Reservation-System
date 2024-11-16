3d80556cb902223cb74500838376fea5bd86bf7c
AltCeDe <jeongmidachaetzu@gmail.com>
2024-11-16 20:28:13 +0800

    ADD0NS
    baka may mali pa check na langs

--- FLIGHT-RESERVATION/Flight Booking/FlightBooking-AddOns/AddOns.Designer.cs
+++ FLIGHT-RESERVATION/Flight Booking/FlightBooking-AddOns/AddOns.Designer.cs
@@ -28,10 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddOns));
+            this.picBaggage = new System.Windows.Forms.PictureBox();
+            this.picFood = new System.Windows.Forms.PictureBox();
+            this.picServices = new System.Windows.Forms.PictureBox();
+            this.btnContinue = new System.Windows.Forms.Button();
+            this.lblAddOns = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lblBaggage = new System.Windows.Forms.Label();
+            this.lblFood = new System.Windows.Forms.Label();
+            this.lblServices = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBaggage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFood)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picServices)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBaggage
+            // 
+            this.picBaggage.BackColor = System.Drawing.Color.Transparent;
+            this.picBaggage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBaggage.BackgroundImage")));
+            this.picBaggage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBaggage.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.picBaggage.Location = new System.Drawing.Point(20, 89);
+            this.picBaggage.Name = "picBaggage";
+            this.picBaggage.Size = new System.Drawing.Size(262, 327);
+            this.picBaggage.TabIndex = 0;
+            this.picBaggage.TabStop = false;
+            // 
+            // picFood
+            // 
+            this.picFood.BackColor = System.Drawing.Color.Transparent;
+            this.picFood.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picFood.BackgroundImage")));
+            this.picFood.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picFood.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.picFood.Location = new System.Drawing.Point(307, 89);
+            this.picFood.Name = "picFood";
+            this.picFood.Size = new System.Drawing.Size(262, 327);
+            this.picFood.TabIndex = 1;
+            this.picFood.TabStop = false;
+            // 
+            // picServices
+            // 
+            this.picServices.BackColor = System.Drawing.Color.Transparent;
+            this.picServices.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picServices.BackgroundImage")));
+            this.picServices.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picServices.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.picServices.Location = new System.Drawing.Point(592, 89);
+            this.picServices.Name = "picServices";
+            this.picServices.Size = new System.Drawing.Size(262, 327);
+            this.picServices.TabIndex = 2;
+            this.picServices.TabStop = false;
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.BackColor = System.Drawing.Color.Transparent;
+            this.btnContinue.BackgroundImage = global::FLIGHT_RESERVATION.Properties.Resources.BUTTON;
+            this.btnContinue.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnContinue.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnContinue.FlatAppearance.BorderSize = 0;
+            this.btnContinue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContinue.Font = new System.Drawing.Font("Kantumruy Pro", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContinue.ForeColor = System.Drawing.Color.White;
+            this.btnContinue.Location = new System.Drawing.Point(471, 467);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(210, 59);
+            this.btnContinue.TabIndex = 3;
+            this.btnContinue.Text = "CONTINUE";
+            this.btnContinue.UseVisualStyleBackColor = false;
+            // 
+            // lblAddOns
+            // 
+            this.lblAddOns.AutoSize = true;
+            this.lblAddOns.Font = new System.Drawing.Font("Kantumruy Pro", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddOns.Location = new System.Drawing.Point(381, 17);
+            this.lblAddOns.Name = "lblAddOns";
+            this.lblAddOns.Size = new System.Drawing.Size(134, 45);
+            this.lblAddOns.TabIndex = 4;
+            this.lblAddOns.Text = "AddOns";
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Transparent;
+            this.btnBack.BackgroundImage = global::FLIGHT_RESERVATION.Properties.Resources.BUTTON2;
+            this.btnBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBack.FlatAppearance.BorderSize = 0;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Kantumruy Pro", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.Color.BlueViolet;
+            this.btnBack.Location = new System.Drawing.Point(215, 467);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(210, 59);
+            this.btnBack.TabIndex = 5;
+            this.btnBack.Text = "BACK";
+            this.btnBack.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(45, 114);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 167);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(332, 114);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(216, 167);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(617, 114);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(216, 167);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // lblBaggage
+            // 
+            this.lblBaggage.AutoSize = true;
+            this.lblBaggage.Font = new System.Drawing.Font("Kantumruy Pro", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBaggage.Location = new System.Drawing.Point(75, 293);
+            this.lblBaggage.Name = "lblBaggage";
+            this.lblBaggage.Size = new System.Drawing.Size(160, 45);
+            this.lblBaggage.TabIndex = 9;
+            this.lblBaggage.Text = "BAGGAGE";
+            // 
+            // lblFood
+            // 
+            this.lblFood.AutoSize = true;
+            this.lblFood.Font = new System.Drawing.Font("Kantumruy Pro", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFood.Location = new System.Drawing.Point(390, 292);
+            this.lblFood.Name = "lblFood";
+            this.lblFood.Size = new System.Drawing.Size(103, 45);
+            this.lblFood.TabIndex = 10;
+            this.lblFood.Text = "FOOD";
+            // 
+            // lblServices
+            // 
+            this.lblServices.AutoSize = true;
+            this.lblServices.Font = new System.Drawing.Font("Kantumruy Pro", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblServices.Location = new System.Drawing.Point(645, 293);
+            this.lblServices.Name = "lblServices";
+            this.lblServices.Size = new System.Drawing.Size(166, 45);
+            this.lblServices.TabIndex = 11;
+            this.lblServices.Text = "SERVICES";
+            // 
+            // AddOns
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblServices);
+            this.Controls.Add(this.lblFood);
+            this.Controls.Add(this.lblBaggage);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.lblAddOns);
+            this.Controls.Add(this.btnContinue);
+            this.Controls.Add(this.picServices);
+            this.Controls.Add(this.picFood);
+            this.Controls.Add(this.picBaggage);
+            this.Name = "AddOns";
+            this.Size = new System.Drawing.Size(871, 571);
+            ((System.ComponentModel.ISupportInitialize)(this.picBaggage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFood)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picServices)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picBaggage;
+        private System.Windows.Forms.PictureBox picFood;
+        private System.Windows.Forms.PictureBox picServices;
+        private System.Windows.Forms.Button btnContinue;
+        private System.Windows.Forms.Label lblAddOns;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lblBaggage;
+        private System.Windows.Forms.Label lblFood;
+        private System.Windows.Forms.Label lblServices;
     }
 }
