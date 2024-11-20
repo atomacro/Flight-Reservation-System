66cf483ed5a3a4f677752ee618abb6f4bd4f5990
JohnJhayAdriano <150686435+JohnJhayAdriano@users.noreply.github.com>
2024-11-20 21:18:21 +0800

    FlightBookings-(CardDetails)
    pre di ko ginawa yung buttons sa taas tapos may nagawa ako kaninang branch yung card payment mali yun

---
+++ FLIGHT-RESERVATION/Flight Booking/FlightBookings-(CardDetails)/CardDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FLIGHT_RESERVATION.Flight_Booking.FlightBookings__CardDetails_
+{
+    public partial class CardDetails : UserControl
+    {
+        public CardDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void roundedTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearchFlight_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
