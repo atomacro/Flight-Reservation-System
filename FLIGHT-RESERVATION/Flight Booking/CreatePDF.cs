dd08f76212fa377b65f2a035799b23d75b7afc45
vivrelavie <stefaniegabion07@gmail.com>
2024-12-01 18:19:16 +0800

    feat: initial commit class: sendEmail and createPDF

---
+++ FLIGHT-RESERVATION/Flight Booking/CreatePDF.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLIGHT_RESERVATION.Flight_Booking
+{
+    internal class CreatePDF
+    {
+
+    }
+}
